fb8f4422a7f34c65ecada42bb8554f99a5b250b2
Kai Light <lastgrunger@gmail.com>
2023-03-24 04:10:44 +0300

    trying to fix callbacks

--- components/RealChart.tsx
+++ components/RealChart.tsx
@@ -16,7 +16,9 @@ import { socket } from "@/socket";
 export default function Chart() {
 
   const [isConnected, setIsConnected] = useState(socket.connected);
-  const [fooEvents, setFooEvents] = useState([] as Array<any>);
+  const [rawData, setRawData] = useState([] as Array<any>);
+  const [rawValues, setRawValues] = useState([0] as Array<any>);
+  const [counter, setCounter] = useState(0)
 
   socket.connect()
 
@@ -33,21 +35,45 @@ export default function Chart() {
       setIsConnected(false);
     }
 
-    function onFooEvent(value:any) {
-      setFooEvents( (previous:any) => [...previous, value]);
+    function onEventsEvent(value:any) {
+      setRawData( (previous:any) => [...previous, value]);
     }
 
     socket.on('connect', onConnect);
     socket.on('disconnect', onDisconnect);
-    socket.on('foo', onFooEvent);
+    socket.on('events', onEventsEvent);
 
     return () => {
       socket.off('connect', onConnect);
       socket.off('disconnect', onDisconnect);
-      socket.off('foo', onFooEvent);
+      socket.off('foo', onEventsEvent);
     };
 
   })
+
+  let timeout:any
+  useEffect( () => {
+
+    const stop = () => {
+      clearTimeout(timeout)
+    }
+
+    timeout = setTimeout( () => {
+      const nextVal = [0,1,2,3,4,5,6,7,8,9,10].at(counter+1) as number
+      if (!nextVal) {
+        stop()
+      }
+      console.info('nextVal', nextVal);
+      setRawValues((previous:any) => [...previous, nextVal])
+
+      console.info('rawValues', rawValues);
+      setCounter(counter+1)
+      if (counter >= 10) {
+        stop()
+      }
+    }, 1000 )
+  }, [counter])
+
 
   const labels = [
     0,1,2,3,4,5,6,7,8,9,10
@@ -81,30 +107,9 @@ export default function Chart() {
     },
   }
 
-  const [values, setValues] = useState( [0,0,0,0,0,0,0,0,0,0] )
-
-  let counter = 1
+  const [values, setValues] = useState( [] as Array<any> )
 
   let interval:any = undefined
-
-  /*
-  interval = setInterval( () => {
-    const nextVal = [0,1,2,3,4,5,6,7,8,9,10].at(counter) as number
-    console.info('nextVal', nextVal);
-    const newVals = [...values]
-    newVals[counter] = nextVal
-    console.info(newVals);
-    // setValues(newVals)
-    counter++
-    if (counter === 10) {
-      stop()
-    }
-  }, 1000 )
-  */
-
-  const stop = () => {
-    clearInterval(interval)
-  }
 
   const datasets = [
     {
